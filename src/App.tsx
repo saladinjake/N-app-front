a4cf0f891bccee6a4f5f5a8bbe3d2fe6367db074
victorjuwa <victor.juwa@kuda.com>
2023-06-13 12:31:30 +0100

    initalize tyscript app

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+
+      <div className="mt-5">
+      
+          <div className="container-fluid">
+            <div className="d-flex flex-row">
+              <div className="col-sm-12">
+                build something here...
+              </div>
+            </div>
+          </div>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
