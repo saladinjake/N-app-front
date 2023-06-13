ae1db7680b4018df72c35ee170052015db85e909
victorjuwa <victor.juwa@kuda.com>
2023-06-13 13:48:28 +0100

    chore: header components

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,28 @@
-import './App.css';
+import NavBar from "components/header";
+import "./App.css";
+import { BrowserRouter, Routes as Switch, Route, NavLink } from "react-router-dom";
 
-
-
+const Element  = () =>{
+  return  <>Hello world</>
+}
 function App() {
   return (
-    <div className="App">
-
-      <div className="mt-5">
-      
-          <div className="container-fluid">
-            <div className="d-flex flex-row">
-              <div className="col-sm-12">
-                build something here...
+    <BrowserRouter>
+      <div className="App">
+        <NavBar title="WP-CART" />
+        <div className="mt-5">
+         
+            <div className="container-fluid">
+              <div className="d-flex flex-row">
+              <Switch>
+                  <Route path="/"  element={Element} />
+                </Switch>
               </div>
             </div>
-          </div>
-      
+         
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
